681c5ff01d878dfbf6e3664511c7cc25032a172c
abdul <abdul.tanveer@gmail.com>
2024-02-21 12:50:28 +0530

    constructor, this

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
        Student abdul;
-       abdul = new Student();
+       abdul = new Student("ansari",123,true);
+        System.out.println(abdul.name);
+
+       Student  san = new Student("sanidhya",111,true);
+        System.out.println("\nname is"+abdul.name);
+        System.out.println(Student.COLLEGE_NAME);
+        int sum = Util.add(abdul.age, san.age);
+        System.out.println("sum  of ages  is--"+sum);
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,28 @@
 public class Student {
-
+public static String COLLEGE_NAME = "IIT";
     String name;
     int age;
     boolean isEligible;
+
+    //pranav
+    public Student(String name, int age, boolean isEligible) {
+        this.name = name;
+        this.age = age;
+        this.isEligible = isEligible;
+    }
+
+    //bhoomika
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.isEligible = true;
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public void display(){
+        System.out.println(this.name);
+    }
 }
