702a1928c3fb63e877cc9a6e931d50e802d4e7d6
abdul <abdul.tanveer@gmail.com>
2024-02-22 10:56:49 +0530

    exception propogation

---
+++ src/exceptions/TestExceptionPropagation2.java
@@ -0,0 +1,24 @@
+package exceptions;
+
+import java.io.IOException;
+
+class TestExceptionPropagation2{
+  void m()throws IOException {
+      int  c = 10 +  20;
+      throw new java.io.IOException("device error");//checked exception
+  }  
+  void n()  throws IOException{
+
+      m();
+  }  
+  void p(){  
+   try{  
+    n();  
+   }catch(Exception e){System.out.println("exception handeled");}  
+  }  
+  public static void main(String args[]){  
+   TestExceptionPropagation2 obj=new TestExceptionPropagation2();  
+   obj.p();  
+   System.out.println("normal flow");  
+  }  
+}  
--- src/exceptions/ThrowEx.java
+++ src/exceptions/ThrowEx.java
@@ -1,10 +1,15 @@
 package exceptions;
 
+import java.io.FileNotFoundException;
+
 public class ThrowEx {
 
-    public static void validate(int  age){
+    public static void validate(int  age) throws UserDefinedException {
         if(age < 18){
-            throw new ArithmeticException("you're  age  should be 18 to vote");
+           // throw new ArithmeticException("you're  age  should be 18 to vote");
+            throw new UserDefinedException("user defined exception");
+                    //FileNotFoundException();
+
         }
         else {
             System.out.println("you can vote");
@@ -14,8 +19,10 @@ public class ThrowEx {
     public static void main(String[] args) {
         try {
             validate(13);
-        } catch (Exception e) {
-            System.out.println("call received");
+        } catch (UserDefinedException e) {
+            throw new RuntimeException(e);
         }
+
+
     }
 }
