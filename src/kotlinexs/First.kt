e6b95b59a383e2ab36957fdfbb44624722f5ac6d
abdul <abdul.tanveer@gmail.com>
2024-02-26 10:08:36 +0530

    kotlin exs

---
+++ src/kotlinexs/First.kt
@@ -0,0 +1,20 @@
+package kotlinexs
+
+import jtpoint.TStudent
+
+class First {
+}
+
+fun main() {
+   // String msg = "how are you";
+    var  msg:String  = "how are you"
+    var age = 20
+    println("hello kotlin-"+msg)
+
+    var tEmp = Employee("abc",123,true,"blr")
+    var mStd  = TStudent(12,"tstudent",43.0f)
+
+    println("name is  "+tEmp.name)
+
+
+}
